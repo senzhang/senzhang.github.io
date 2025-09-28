2f7df9673728d305317deaf3a0c8b017d080a41d
SenZhang <sen.zhang@live.com>
2025-09-28 16:27:50 -0400

    init.

---
+++ script.js
@@ -0,0 +1,28 @@
+const texts = [
+  "c# Developer",
+  ".net Developer",
+  "Azure Developer",
+];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+const typingEl = document.getElementById("typing");
+
+function type() {
+  if (count === texts.length) count = 0;
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  typingEl.textContent = letter;
+  if (letter.length === currentText.length) {
+    setTimeout(() => {
+      index = 0;
+      count++;
+      setTimeout(type, 500);
+    }, 1500);
+  } else {
+    setTimeout(type, 120);
+  }
+}
+type();
